627a055bbb0f2529199f05c9ea4a22e62b5deb43
rayray <rayray@Ubuntu.myguest.virtualbox.org>
2024-10-02 13:28:32 -0600

    encode-json-v6

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -3,7 +3,7 @@ package main
 import (
 	// "fmt"
 	// "net/http"
-	"encoding/json"
+	// "encoding/json"
 	"net/http"
 )
 
@@ -20,14 +20,16 @@ func (a *applicationDependencies) healthCheckHandler(w http.ResponseWriter, r *h
 			"version":     appVersion,
 		},
 	}
-	jsResponse, err := json.Marshal(data)
+	//jsResponse, err := json.Marshal(data)
+	err := a.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		a.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
+		// a.logger.Error(err.Error())
+		// http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		// return
+		a.serverErrorResponse(w, r, err)
 	}
-	jsResponse = append(jsResponse, '\n')
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsResponse)
+	// jsResponse = append(jsResponse, '\n')
+	// w.Header().Set("Content-Type", "application/json")
+	// w.Write(jsResponse)
 
 }
