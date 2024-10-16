3a6d5b900c83c958766e2d3c9abfd01fa99ab035
rayray <rayray@Ubuntu.myguest.virtualbox.org>
2024-10-16 13:13:22 -0600

    connection-pool-setup

--- cmd/api/comments.go
+++ cmd/api/comments.go
@@ -7,22 +7,18 @@ import (
 	//"github.com/RayMC17/comments/internal/data"
 )
 
-func (a *applicationDependencies)createCommentHandler(w http.ResponseWriter, r *http.Request) {
+func (a *applicationDependencies) createCommentHandler(w http.ResponseWriter, r *http.Request) {
 	var incomingData struct {
-	Content string `json:"content"`
-	Author string `json:"author"`
+		Content string `json:"content"`
+		Author  string `json:"author"`
 	}
 
 	//err := json.NewDecoder (r.Body) .Decode(&incomingData)
 	err := a.readJSON(w, r, &incomingData)
-if err != nil {
-	a.badRequestResponse(w, r, err)
-	//a.errorResponseJSON(w, r, http.StatusBadRequest, err.Error())
-return
+	if err != nil {
+		a.badRequestResponse(w, r, err)
+		//a.errorResponseJSON(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", incomingData)
 }
-fmt.Fprintf(w, "%+v\n", incomingData)
-}
-
-
-
-
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const appVersion = "1.0.0"
@@ -14,6 +18,9 @@ const appVersion = "1.0.0"
 type serverConfig struct {
 	port        int
 	environment string
+	db          struct {
+		dsn string
+	}
 }
 
 type applicationDependencies struct {
@@ -26,9 +33,18 @@ func main() {
 	var settings serverConfig
 	flag.IntVar(&settings.port, "port", 4000, "server port")
 	flag.StringVar(&settings.environment, "env", "development", "Environment?(development|staging|production)")
+	flag.StringVar(&settings.db.dsn, "db-dsn", "postgres://comments:fishsticks@localhost/comments", "PostgresSQL DSN")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(settings)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	logger.Info("database connection pool established")
 
 	appInstance := &applicationDependencies{
 		config: settings,
@@ -45,7 +61,22 @@ func main() {
 		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 	logger.Info("starting server", "address", apiServer.Addr, "environment", settings.environment)
-	err := apiServer.ListenAndServe()
+	err = apiServer.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(settings serverConfig) (*sql.DB, error) {
+	db, err := sql.Open("postgres", settings.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
 }
