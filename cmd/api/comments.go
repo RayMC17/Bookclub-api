03ade165b52b7c72ff648f5255f9c1a277230144
rayray <rayray@Ubuntu.myguest.virtualbox.org>
2024-10-19 13:00:16 -0600

    parse-json-v4

--- cmd/api/comments.go
+++ cmd/api/comments.go
@@ -4,13 +4,28 @@ import (
 	//"encoding/json"
 	"fmt"
 	"net/http"
-	//"github.com/RayMC17/comments/internal/data"
+
+	"github.com/RayMC17/comments/internal/data"
+	"github.com/RayMC17/comments/internal/validator"
 )
 
 func (a *applicationDependencies) createCommentHandler(w http.ResponseWriter, r *http.Request) {
 	var incomingData struct {
 		Content string `json:"content"`
 		Author  string `json:"author"`
+	}
+
+	comment := &data.Comment{
+		Content: incomingData.Content,
+		Author:  incomingData.Author,
+	}
+
+	v := validator.New()
+
+	data.ValidateComment(v, comment)
+	if !v.IsEmpty() {
+		a.failedValidationResponse(w, r, v.Errors)
+		return
 	}
 
 	//err := json.NewDecoder (r.Body) .Decode(&incomingData)
